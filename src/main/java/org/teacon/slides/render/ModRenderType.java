ddad0d1429d9fc066a2981ea17bb0eff234b01f4
FledgeXu <xzk0701@gmail.com>
2020-10-22 16:30:11 +0800

    Add a feature which can show Projector when player holds a stick.

---
+++ src/main/java/org/teacon/slides/render/ModRenderType.java
@@ -0,0 +1,30 @@
+package org.teacon.slides.render;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import org.lwjgl.opengl.GL11;
+
+import java.util.OptionalDouble;
+
+// Source: https://github.com/McJty/YouTubeModding14/blob/master/src/main/java/com/mcjty/mytutorial/client/MyRenderType.java
+public class ModRenderType extends RenderType {
+
+    public ModRenderType(String nameIn, VertexFormat formatIn, int drawModeIn, int bufferSizeIn, boolean useDelegateIn, boolean needsSortingIn, Runnable setupTaskIn, Runnable clearTaskIn) {
+        super(nameIn, formatIn, drawModeIn, bufferSizeIn, useDelegateIn, needsSortingIn, setupTaskIn, clearTaskIn);
+    }
+
+    private static final LineState THICK_LINES = new LineState(OptionalDouble.of(3.0D)); // Set the line's width in here.
+
+    public static final RenderType OVERLAY_LINES = makeType("overlay_lines",
+            DefaultVertexFormats.POSITION_COLOR, GL11.GL_LINES, 256,
+            RenderType.State.getBuilder().line(THICK_LINES)
+                    .layer(PROJECTION_LAYERING)
+                    .transparency(TRANSLUCENT_TRANSPARENCY)
+                    .texture(NO_TEXTURE)
+                    .depthTest(DEPTH_ALWAYS)
+                    .cull(CULL_DISABLED)
+                    .lightmap(LIGHTMAP_DISABLED)
+                    .writeMask(COLOR_WRITE)
+                    .build(false));
+}
---
+++ src/main/java/org/teacon/slides/render/ProjectorWorldRender.java
@@ -0,0 +1,94 @@
+package org.teacon.slides.render;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Matrix4f;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.teacon.slides.ProjectorTileEntity;
+
+//Source: https://github.com/McJty/YouTubeModding14/blob/master/src/main/java/com/mcjty/mytutorial/client/InWorldRenderer.java
+@Mod.EventBusSubscriber(Dist.CLIENT)
+public class ProjectorWorldRender {
+    @SubscribeEvent
+    public static void worldRender(final RenderWorldLastEvent event) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player.getHeldItemMainhand().getItem() == Items.STICK) {
+            locateProjectorTileEntities(player, event.getMatrixStack());
+        }
+    }
+
+    //To draw the line.
+    private static void drawLine(IVertexBuilder builder, Matrix4f positionMatrix, BlockPos pos, float dx1, float dy1, float dz1, float dx2, float dy2, float dz2, float red, float green, float blue, float alpha) {
+        builder.pos(positionMatrix, pos.getX() + dx1, pos.getY() + dy1, pos.getZ() + dz1)
+                .color(red, green, blue, alpha)
+                .endVertex();
+        builder.pos(positionMatrix, pos.getX() + dx2, pos.getY() + dy2, pos.getZ() + dz2)
+                .color(red, green, blue, alpha)
+                .endVertex();
+    }
+
+    private static void drawBlueLine(IVertexBuilder builder, Matrix4f positionMatrix, BlockPos pos, float dx1, float dy1, float dz1, float dx2, float dy2, float dz2) {
+        drawLine(builder, positionMatrix, pos, dx1, dy1, dz1, dx2, dy2, dz2, 1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    private static void locateProjectorTileEntities(ClientPlayerEntity player, MatrixStack matrixStack) {
+        IRenderTypeBuffer.Impl buffer = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+        IVertexBuilder builder = buffer.getBuffer(ModRenderType.OVERLAY_LINES);
+
+        BlockPos playerPos = player.getPosition();
+        int px = playerPos.getX();
+        int py = playerPos.getY();
+        int pz = playerPos.getZ();
+        World world = player.getEntityWorld();
+
+        matrixStack.push();
+
+        Vec3d projectedView = Minecraft.getInstance().gameRenderer.getActiveRenderInfo().getProjectedView();
+        matrixStack.translate(-projectedView.x, -projectedView.y, -projectedView.z); // In default situation, the original position of matrix in world origin point. We should translate it.
+
+        Matrix4f matrix = matrixStack.getLast().getMatrix();
+
+        BlockPos.Mutable pos = new BlockPos.Mutable();
+        for (int dx = -10; dx <= 10; dx++) {
+            for (int dy = -10; dy <= 10; dy++) {
+                for (int dz = -10; dz <= 10; dz++) {
+                    pos.setPos(px + dx, py + dy, pz + dz);
+                    //Check ProjectorTileEntity
+                    if (world.getTileEntity(pos) != null && world.getTileEntity(pos).getType() == ProjectorTileEntity.theType) {
+                        //For a Block, there are twelve lines should be drawn.
+                        drawBlueLine(builder, matrix, pos, 0, 0, 0, 1, 0, 0);
+                        drawBlueLine(builder, matrix, pos, 0, 1, 0, 1, 1, 0);
+                        drawBlueLine(builder, matrix, pos, 0, 0, 1, 1, 0, 1);
+                        drawBlueLine(builder, matrix, pos, 0, 1, 1, 1, 1, 1);
+
+                        drawBlueLine(builder, matrix, pos, 0, 0, 0, 0, 0, 1);
+                        drawBlueLine(builder, matrix, pos, 1, 0, 0, 1, 0, 1);
+                        drawBlueLine(builder, matrix, pos, 0, 1, 0, 0, 1, 1);
+                        drawBlueLine(builder, matrix, pos, 1, 1, 0, 1, 1, 1);
+
+                        drawBlueLine(builder, matrix, pos, 0, 0, 0, 0, 1, 0);
+                        drawBlueLine(builder, matrix, pos, 1, 0, 0, 1, 1, 0);
+                        drawBlueLine(builder, matrix, pos, 0, 0, 1, 0, 1, 1);
+                        drawBlueLine(builder, matrix, pos, 1, 0, 1, 1, 1, 1);
+                    }
+                }
+            }
+        }
+
+        matrixStack.pop();
+
+        RenderSystem.disableDepthTest();
+        buffer.finish(ModRenderType.OVERLAY_LINES);
+    }
+}
