1abe7f176fa4a32e84671c268193eb7b15d0f068
Yanbing Zhao <zzzz.mail.ustc@gmail.com>
2026-05-11 00:06:38 +0800

    Fix projector item defaults on 26.1

--- src/main/java/org/teacon/slides/item/ProjectorItem.java
+++ src/main/java/org/teacon/slides/item/ProjectorItem.java
@@ -3,8 +3,6 @@ package org.teacon.slides.item;
 import com.mojang.logging.annotations.FieldsAreNonnullByDefault;
 import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.NonNullList;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
@@ -12,29 +10,36 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.Util;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.teacon.slides.ModRegistries;
-import org.teacon.slides.block.ProjectorBlock;
 import org.teacon.slides.block.ProjectorBlockEntity;
 import org.teacon.slides.inventory.ProjectorContainerMenu;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @FieldsAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 public final class ProjectorItem extends BlockItem {
+    public static final ItemContainerContents DEFAULT_CONTAINER_ITEMS = Util.make(() -> {
+        var list = new ArrayList<Optional<ItemStackTemplate>>(1);
+        var item = ModRegistries.SLIDE_ITEM.get();
+        list.add(Optional.of(new ItemStackTemplate(item)));
+        return new ItemContainerContents(list);
+    });
 
     public ProjectorItem(Identifier identifier) {
         super(ModRegistries.PROJECTOR_BLOCK.get(), new Properties()
                 .rarity(Rarity.RARE)
-                .component(DataComponents.CONTAINER, ItemContainerContents.EMPTY)
+                .component(DataComponents.CONTAINER, DEFAULT_CONTAINER_ITEMS)
                 .useBlockDescriptionPrefix()
                 .setId(ResourceKey.create(Registries.ITEM, identifier)));
     }
